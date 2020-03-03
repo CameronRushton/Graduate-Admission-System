91d2c41c1aaf16af5d2c490a070a960d1e032cc8
cameronrushton <cameron.rushton@rogers.com>
2020-03-03 00:54:16 -0500

    added UI to static

--- src/main/javascript/src/environment.js
+++ src/main/javascript/src/environment.js
@@ -1,5 +1,5 @@
 export default {
-  debug: true,
-  testing: true,
-  host: "http://localhost:8080" //"http://crushtonaddressbook.herokuapp.com/"
+  debug: false,
+  testing: false,
+  host: "http://localhost:8080/"
 };
