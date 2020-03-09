23efa77a01518d7ae30bb84d29d335e0e527be79
cameronrushton <cameron.rushton@rogers.com>
2020-03-08 20:27:03 -0400

    find out current dir

--- src/main/javascript/deploy.js
+++ src/main/javascript/deploy.js
@@ -60,16 +60,3 @@ function copyFolderRecursiveSync( source, target ) {
     }
 }
 
-//var directory = "../resources/static"
-//fs.readdir(directory, (err, files) => {
-//  if (err) throw err;
-//
-//  for (const file of files) {
-//    fs.unlink(path.join(directory, file), err => {
-//      if (err) throw err;
-//    });
-//  }
-//});
-
-// copyFolderRecursiveSync("./dist", "../resources/static")
-
