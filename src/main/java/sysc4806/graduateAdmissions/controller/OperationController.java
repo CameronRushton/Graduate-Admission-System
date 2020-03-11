d1710f456f6b1a89f5ed314beb0a141882eb3d87
Eric <ericbedard@cmail.carleton.ca>
2020-03-11 16:47:51 -0400

    add missing controllers

---
+++ src/main/java/sysc4806/graduateAdmissions/controller/OperationController.java
@@ -0,0 +1,26 @@
+package sysc4806.graduateAdmissions.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import sysc4806.graduateAdmissions.model.Operation;
+
+/**
+ * Controller to retrieve Operations
+ *
+ * @author Eric
+ */
+@RestController
+public class OperationController {
+
+    /**
+     * Provides Operation in the system as JSON
+     *
+     * @return JSON containing every Operation
+     */
+    @GetMapping("/operations/")
+    public ResponseEntity getOperations() {
+        return ResponseEntity.status(HttpStatus.OK).body(Operation.values());
+    }
+}
