713bd32fe2fcc989a08912cce334f1af6b3bbbe8
LukeANewton <lukeantonynewton@gmail.com>
2020-03-11 11:35:50 -0400

    add sign in/out links in login page

--- src/main/javascript/src/pages/login/login.js
+++ src/main/javascript/src/pages/login/login.js
@@ -5,6 +5,8 @@ export class Login {
     }
 
     attached() {
+    	$("head").append("<script src='https://apis.google.com/js/platform.js' async defer></script>");
+		$("head").append("<meta name='google-signin-client_id' content='787575027862-t2vb0ae8ftk68nr9br9s4untp9e6t614.apps.googleusercontent.com'>");
     }
 
     scrollFn() {
@@ -12,5 +14,20 @@ export class Login {
         document.getElementById("top").scrollIntoView({ 
             behavior: 'smooth'
         });
+    }
+
+    onSignOut() {
+		var auth2 = gapi.auth2.getAuthInstance();
+		auth2.signOut().then(function () {
+			console.log('User signed out.');
+		});
+	}
+
+	onSignIn(googleUser) {
+    	var profile = googleUser.getBasicProfile();
+    	console.log('ID: ' + profile.getId()); // Do not send to your backend! Use an ID token instead.
+    	console.log('Name: ' + profile.getName());
+    	console.log('Image URL: ' + profile.getImageUrl());
+    	console.log('Email: ' + profile.getEmail()); // This is null if the 'email' scope is not present.
     }
 }
