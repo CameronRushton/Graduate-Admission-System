4e38d80765462d7655cf5086425fae265fd6b466
LukeANewton <lukeantonynewton@gmail.com>
2020-03-22 13:54:55 -0400

    remove unused log from main class

--- src/main/java/sysc4806/graduateAdmissions/GraduateAdmissions.java
+++ src/main/java/sysc4806/graduateAdmissions/GraduateAdmissions.java
@@ -1,11 +1,9 @@
 package sysc4806.graduateAdmissions;
 
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-@Slf4j
 public class GraduateAdmissions {
     public static void main(String[] args) {
         new SpringApplicationBuilder(GraduateAdmissions.class)
